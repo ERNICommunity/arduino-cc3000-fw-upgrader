787d8acebefe9ee8fb0e1b9e2393c254d15b5828
Dieter Niklaus <dieter.niklaus@gmx.net>
2015-02-06 22:55:44 +0100

    Added initial source code content.
    Signed-off-by: Dieter Niklaus <dieter.niklaus@gmx.net>

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ arduino-cc3000-fw-upgrader
 ==========================
 
 Adafruit CC3000 Firmware V1.13 Upgrade Sketch
+(based on: https://github.com/adafruit/Adafruit_CC3000_Library/tree/master/examples/driverpatch_1_13)
+
+For intructions see:
+* https://learn.adafruit.com/adafruit-cc3000-wifi/firmware-upgrades
+
---
+++ src/arduino_cc3000_fw_upgrader.h
@@ -0,0 +1,29 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _arduino_cc3000_fw_upgrader_H_
+#define _arduino_cc3000_fw_upgrader_H_
+#include "Arduino.h"
+//add your includes for the project arduino_cc3000_fw_upgrader here
+
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project arduino_cc3000_fw_upgrader here
+
+
+
+
+//Do not add code below this line
+#endif /* _arduino_cc3000_fw_upgrader_H_ */
